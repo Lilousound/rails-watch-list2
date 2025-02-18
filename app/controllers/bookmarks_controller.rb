b3bd1f4214e6f5b46df94e70f1b09c0073d193e3
Aurelie Raout <aurelieraout@gmail.com>
2025-02-18 18:22:45 +0100

    created controllers add scss style

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,19 @@
+class BookmarksController < ApplicationController
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.save
+    redirect_to bookmarks_path(@bookmark)
+  end
+end
