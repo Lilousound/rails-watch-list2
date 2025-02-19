86368f37f9088cc648fd003d8bfc1295e7498eb7
Aurelie Raout <aurelieraout@gmail.com>
2025-02-19 17:10:24 +0100

    going nowhere

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   root to: "lists#index"
 
-  resources :movies, only: [ :index ] do
-    resources :bookmarks, only: [ :create, :new ]
+  resources :movies, only: [:index, :new] do
+    resources :bookmarks, only: [:create, :new]
   end
   resources :lists
 
-  resources :bookmarks, only: [ :destroy ]
+  resources :bookmarks, only: [:destroy]
 end
