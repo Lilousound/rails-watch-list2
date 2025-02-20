2a6748a752f584dd60e1c3b2c6a14eb2cac222d1
Aurelie Raout <aurelieraout@gmail.com>
2025-02-20 17:28:01 +0100

    progress made. Almost complete. Fonctionnal app

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,4 +1,5 @@
 class BookmarksController < ApplicationController
+
   def index
     @bookmarks = Bookmark.all
   end
@@ -9,11 +10,34 @@ class BookmarksController < ApplicationController
 
   def new
     @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
   end
 
   def create
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
-    @bookmark.save
-    redirect_to bookmarks_path(@bookmark)
+    @movies = Movie.where(id: params[:bookmark][:movie_id])
+    @movies.each do |movie|
+      @bookmark = Bookmark.new(list: @list, movie: movie, comment: bookmark_params[:comment])
+      @bookmark.save
+    end
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   root to: "lists#index"
-
-  resources :movies, only: [:index, :new] do
+  resources :lists do
     resources :bookmarks, only: [:create, :new]
   end
-  resources :lists
 
   resources :bookmarks, only: [:destroy]
 end
